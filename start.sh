dd19079af17e6caaebe04696ce63369f7b2f316f
SHUU0801 <71332555+SHUU0801@users.noreply.github.com>
2026-02-24 13:31:24 +0900

    [Fix] Use python3 and PORT env for Render

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Render上でのバックグラウンドクローラーとWebサーバーの同時起動
-python crawler.py &
-gunicorn server:app
+# Render上でのクローラー + Webサーバー同時起動
+python3 crawler.py &
+gunicorn server:app --bind 0.0.0.0:${PORT:-5000}
